ecc3e86958d1522cd130f313f8aca072b7273a50
Renan <88853399+renanpd95@users.noreply.github.com>
2022-04-14 23:30:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+np = int(input("Número de partidas do campeonato: "))
+ng = int(input("Número de gols marcados em todo campeonato: "))
+
+media = ng/np
+
+print("Média de gols por partidas: ",media)
